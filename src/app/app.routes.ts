517037026352b06dd1cebbac0edcae15c7e2ca0c
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-05 23:49:24 +0700

    feat(routes): config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ProductComponent } from './features/product/product.component';
+import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
+import { LoginComponent } from './core/auth/pages/login/login.component';
+import { SignupComponent } from './core/auth/pages/signup/signup.component';
+import { PasswordRecoveryComponent } from './core/auth/pages/password-recovery/password-recovery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  {
+    path: 'auth',
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'sign-up', component: SignupComponent },
+      { path: 'password-recovery', component: PasswordRecoveryComponent },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
