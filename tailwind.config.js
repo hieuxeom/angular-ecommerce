1b1ace9439d5eae939c653bc32e105a39fa1b683
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-06 14:38:58 +0700

    chore: config extend color tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000",
+        secondary: "#999",
+        disabled: "#666",
+        gray: "#f0f0f0",
+        danger: "#ff3333",
+        success: "#01ab31",
+      },
+    },
   },
   plugins: [],
 };
