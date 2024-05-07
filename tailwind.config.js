b742f50b2d0294c62712e0973f6b32323f4b2f2f
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-07 11:30:09 +0700

    chore: change color key, custom tailwind border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,17 @@ module.exports = {
     extend: {
       colors: {
         primary: "#000",
-        secondary: "#999",
+        secondary: "#ff5b5b",
+        sub: "#999",
         disabled: "#666",
         gray: "#f0f0f0",
         danger: "#ff3333",
         success: "#01ab31",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "2.5rem",
+        "6xl": "3rem",
       },
     },
   },
