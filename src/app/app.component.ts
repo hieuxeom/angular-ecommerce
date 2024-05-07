7d837a22718348fa3250a7c000ae64e763f22bdb
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-07 18:48:28 +0700

    chore: call nav & footer components in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import {
 } from '@angular/router';
 
 import { NavComponent } from './shared/components/nav/nav.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
