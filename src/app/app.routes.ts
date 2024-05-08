8934fe65fecbf445aaba5482d4d7235c050dc5b8
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-09 01:33:15 +0700

    feat(route): add children route for /product route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,23 @@ import { LoginComponent } from './core/auth/pages/login/login.component';
 import { SignupComponent } from './core/auth/pages/signup/signup.component';
 import { PasswordRecoveryComponent } from './core/auth/pages/password-recovery/password-recovery.component';
 import { ProfileComponent } from './features/profile/profile.component';
+import { ProductDetailComponent } from './features/product-detail/product-detail.component';
+import { CommentsSectionComponent } from './features/product-detail/components/comments-section/comments-section.component';
+import { ReviewsSectionComponent } from './features/product-detail/components/reviews-section/reviews-section.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'product', component: ProductComponent },
+  {
+    path: 'product',
+    children: [
+      { path: '', component: ProductComponent },
+      {
+        path: ':productId',
+        component: ProductDetailComponent,
+      },
+    ],
+  },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'profile', component: ProfileComponent },
@@ -25,5 +37,5 @@ export const routes: Routes = [
       { path: '**', redirectTo: 'login', pathMatch: 'full' },
     ],
   },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
