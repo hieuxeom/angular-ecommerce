63a322ae09e24ffd6c3b2250f511dba1d70091c8
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-09 01:35:53 +0700

    chore: add data for product sample data

--- src/app/shared/services/products-data/products-data.service.ts
+++ src/app/shared/services/products-data/products-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 export interface IProduct {
+  productId: string;
   productName: string;
   productPrice: number;
   productImage: string;
@@ -18,6 +19,7 @@ export class ProductsDataService {
   public getAllProducts(): IProduct[] {
     return [
       {
+        productId: 'product-1',
         productName: 'Product 1',
         productPrice: 100,
         productImage: 'assets/product/test-2.png',
@@ -26,6 +28,7 @@ export class ProductsDataService {
         productRating: 4.5,
       },
       {
+        productId: 'product-2',
         productName: 'Product 2',
         productPrice: 400,
         productImage: 'assets/product/test-1.png',
@@ -34,6 +37,7 @@ export class ProductsDataService {
         productRating: 4.3,
       },
       {
+        productId: 'product-3',
         productName: 'Product 3',
         productPrice: 100,
         productImage: 'assets/product/test-2.png',
@@ -42,7 +46,26 @@ export class ProductsDataService {
         productRating: 4.5,
       },
       {
+        productId: 'product-4',
         productName: 'Product 4',
+        productPrice: 1200,
+        productImage: 'assets/product/test-1.png',
+        isDiscount: true,
+        discountPercents: 10,
+        productRating: 3.5,
+      },
+      {
+        productId: 'product-5',
+        productName: 'Product 5',
+        productPrice: 1200,
+        productImage: 'assets/product/test-2.png',
+        isDiscount: true,
+        discountPercents: 10,
+        productRating: 3.5,
+      },
+      {
+        productId: 'product-6',
+        productName: 'Product 6',
         productPrice: 1200,
         productImage: 'assets/product/test-1.png',
         isDiscount: true,
