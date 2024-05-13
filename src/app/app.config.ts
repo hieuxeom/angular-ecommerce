e4410c73d48fca51cecab435e5c767e896da8db5
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-13 09:13:31 +0700

    feat(provider): add provideHttpClient(withFetch())

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch()),
     importProvidersFrom(HttpClientModule),
     provideAnimations(),
   ],
