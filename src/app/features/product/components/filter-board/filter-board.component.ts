438e2cb75236b55e12e5a78baf11bf01be507045
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-27 15:30:19 +0700

    feat: handle active slider price range

--- src/app/features/product/components/filter-board/filter-board.component.ts
+++ src/app/features/product/components/filter-board/filter-board.component.ts
@@ -1,33 +1,69 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  type OnChanges,
+  type SimpleChanges,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { SliderModule, type SliderChangeEvent } from 'primeng/slider';
 
 import { HrComponent } from '../../../../shared/components/hr/hr.component';
 import { CategoryService } from '../../../../shared/services/CategoryServices/category.service';
 import type { ICategory } from '../../../../shared/interfaces/category';
+import type { TypeFilter } from '../../../../shared/interfaces/product';
+import { ProductService } from '../../../../shared/services/ProductServices/product.service';
+import { CheckboxModule } from 'primeng/checkbox';
+
 @Component({
   selector: 'app-filter-board',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, SliderModule, HrComponent],
-  providers: [CategoryService],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    SliderModule,
+    HrComponent,
+    CheckboxModule,
+  ],
+  providers: [CategoryService, ProductService],
   templateUrl: './filter-board.component.html',
   styleUrl: './filter-board.component.css',
 })
 export class FilterBoardComponent {
+  @Input() activeFilter: TypeFilter = 'all';
+
   public minValue: number = 0;
   public maxValue: number = 10000;
   public currentValue: number[] = [50, 1000];
-  public demoColor = Array.from({ length: 10 });
+  public listColors: string[] = [];
 
   public listCategories: ICategory[] = [];
 
-  public constructor(private categoryApiService: CategoryService) {}
+  public filterWithPrice: boolean = false;
+
+  public constructor(
+    private categoryApiService: CategoryService,
+    private productApiService: ProductService,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      if (params['filter']) {
+        this.activeFilter = params['filter'];
+      } else {
+        this.activeFilter = 'all';
+      }
+    });
+  }
 
   public ngOnInit() {
     this.categoryApiService.getAllCategory().subscribe((listCategories) => {
       this.listCategories = listCategories.data;
+    });
+
+    this.productApiService.getAllProductColors().subscribe((response) => {
+      this.listColors = response.data;
     });
   }
 
