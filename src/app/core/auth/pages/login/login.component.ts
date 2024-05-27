c75e4c2eede6cc4a8a9cf6c0f454ebd5cf8b1c89
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-27 15:22:42 +0700

    refactor: change div to form & restyle button, input in auth site

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
@@ -12,45 +19,44 @@ import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, ToastModule, RippleModule],
-  providers: [AuthService, MessageService, Router, CookieService],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ToastModule,
+    RippleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [MessageService, CookieService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  public email: string = '';
-  public password: string = '';
+  public authForm: FormGroup;
 
   constructor(
     private authApiService: AuthService,
     private _messageService: MessageService,
     private _router: Router,
-    private _cookieService: CookieService
-  ) {}
+    private _cookieService: CookieService,
+    private _formBuilder: FormBuilder
+  ) {
+    this.authForm = this._formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   public handleLogin() {
-    if (!this.email) {
+    if (!this.authForm.valid) {
       return this._messageService.add({
         severity: 'error',
         summary: 'Error',
-        detail: 'Email is required',
+        detail: 'Missing email or password field',
       });
     }
 
-    if (!this.password) {
-      return this._messageService.add({
-        severity: 'error',
-        summary: `Error`,
-        detail: 'Password is required',
-      });
-    }
-
-    const loginData: ILogin = {
-      email: this.email,
-      password: this.password,
-    };
-
-    this.authApiService.verifyAccount(loginData).subscribe(
+    this.authApiService.verifyAccount(this.authForm.value).subscribe(
       (response) => {
         if (response.status === 'success') {
           const expiredTime = new Date();
