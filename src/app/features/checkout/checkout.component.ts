68704658c99a21b28b608b3b281220ee14621138
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-05-27 15:46:52 +0700

    refactor: split html of the steps into components

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,42 +1,57 @@
+import { UserService } from './../../shared/services/UserServices/user.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, type SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import type { MenuItem } from 'primeng/api';
 import { DropdownModule } from 'primeng/dropdown';
+import { ListboxModule, type ListboxChangeEvent } from 'primeng/listbox';
 import { StepsModule } from 'primeng/steps';
 import { HrComponent } from '../../shared/components/hr/hr.component';
+import { AddressService } from '../../shared/services/AddressServices/address.service';
+import type { IUserAddress } from '../../shared/interfaces/user';
+import { BillingAddressComponent } from './components/billing-address/billing-address.component';
+import { CompleteOrderComponent } from './components/complete-order/complete-order.component';
+import { RecheckBillComponent } from './components/recheck-bill/recheck-bill.component';
 @Component({
   selector: 'app-checkout',
   standalone: true,
   imports: [
     CommonModule,
     FormsModule,
+    ListboxModule,
     StepsModule,
     DropdownModule,
     HrComponent,
+    BillingAddressComponent,
+    CompleteOrderComponent,
+    RecheckBillComponent,
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent {
-  public step: number = 3;
-  public stepItems: MenuItem[] = [
-    { label: 'Billing Address' },
-    { label: 'Confirm Payment' },
-  ];
+  public step: number = 1;
+  public billingAddress!: IUserAddress;
 
-  public provinces = [
-    { label: 'Hà Nội', value: 'most-popular' },
-    { label: 'Hồ Chí Minh', value: 'price-asc' },
-    { label: 'Cần Thơ', value: 'price-desc' },
-    { label: 'Ninh Thuận', value: 'rating-asc' },
-    { label: 'Khánh Hòa', value: 'rating-desc' },
-  ];
-  public selectedProvince: string = '';
-  public cites: string[] = [];
-  public selectedCity: string = '';
+  constructor(private userService: UserService) {}
 
-  public nextStep() {
+  public onNextStep1(event: any) {
+    if (event) {
+      const { formValue, isSave } = event;
+
+      this.billingAddress = formValue;
+
+      if (isSave) {
+        this.userService.saveNewAddress(formValue).subscribe((response) => {
+          console.log(response);
+        });
+      }
+
+      this.step += 1;
+    }
+  }
+
+  public onNextStep2(event: any) {
+    console.log(event);
     return (this.step += 1);
   }
 }
