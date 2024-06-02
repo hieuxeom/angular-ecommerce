b3a4df0e14a64b5241c0d90cad4176e4d3dbb1da
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-06-03 01:39:34 +0700

    fix: rehandle check isLoggedIn with refresh token

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -6,27 +6,29 @@ import type { IApiResponse } from '../../../shared/interfaces/api';
 import type { IUserAuth } from '../../../shared/interfaces/user';
 import { CookieService } from 'ngx-cookie-service';
 import { FormGroup } from '@angular/forms';
+import { HttpConfigService } from '../../../shared/services/HttpConfig/http-config.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   public API_URL = 'http://localhost:5000/api/auth';
-  public httpOptions = {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${this._cookieService.get('access_token')}`,
-    },
-  };
+  public REFRESH_TOKEN_API_URL: string = 'http://localhost:5000/api/users/rftk';
+
   constructor(
     private httpClient: HttpClient,
-    private _cookieService: CookieService
+    private _cookieService: CookieService,
+    private _httpConfig: HttpConfigService
   ) {}
 
   public isLoggedIn() {
     if (this._cookieService.get('access_token')) {
       return true;
     } else {
+      if (this._cookieService.get('refresh_token')) {
+        this._httpConfig.getNewAccessToken();
+        return true;
+      }
       return false;
     }
   }
@@ -48,8 +50,4 @@ export class AuthService {
       loginData
     );
   }
-
-  // public getRefreshToken() {
-  //   return this.httpClient.get(`${this.API_URL}`, this.httpOptions);
-  // }
 }
